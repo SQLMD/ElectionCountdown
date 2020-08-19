604d670901ceacc1ba749dfb154dc35b04e930ee
Mike Donnelly <sqldonnelly@gmail.com>
2020-08-19 14:05:26 -0700

    Initial Commit

---
+++ frmCountdown.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ElectionCountdown
+{
+    partial class frmCountdown
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCountdown = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCountdown
+            // 
+            this.txtCountdown.Location = new System.Drawing.Point(278, 92);
+            this.txtCountdown.Name = "txtCountdown";
+            this.txtCountdown.Size = new System.Drawing.Size(39, 26);
+            this.txtCountdown.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(191, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "There are ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(319, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(314, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = " days until the next US Presidential Election";
+            // 
+            // frmCountdown
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 232);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCountdown);
+            this.Name = "frmCountdown";
+            this.Text = "Countdown";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCountdown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ frmCountdown.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElectionCountdown
+{
+    public partial class frmCountdown : Form
+    {
+
+        public frmCountdown()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            System.DateTime firstDate = new System.DateTime(2020, 08, 19);
+            System.DateTime secondDate = new System.DateTime(2020, 11, 03);
+
+            var diff = (secondDate - firstDate).TotalDays.ToString();
+            txtCountdown.Text = diff;
+        }
+    }
+}
